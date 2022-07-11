a291c169d19d90cfefce1054e401bcce5401d915
suin <suinyeze@gmail.com>
2022-07-12 06:01:52 +0900

    feat: 🎸 get permalink

--- api/postFeed.ts
+++ api/postFeed.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-// import { WebClient } from "@slack/web-api";
+import { WebClient } from "@slack/web-api";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   console.log(request.body);
@@ -29,7 +29,10 @@ const urlVerification = (request: VercelRequest, response: VercelResponse) => {
   response.status(200).send({ challenge: request.body.challenge });
 };
 
-const handleEvent = (request: VercelRequest, response: VercelResponse) => {
+const handleEvent = async (
+  request: VercelRequest,
+  response: VercelResponse
+) => {
   const { body } = request;
   const { event } = body;
   if (event.type !== "message") {
@@ -43,5 +46,18 @@ const handleEvent = (request: VercelRequest, response: VercelResponse) => {
     return;
   }
   console.log("event handled", JSON.stringify(event));
+
+  const web = new WebClient(process.env["TOKEN"]);
+  const { permalink } = await web.chat.getPermalink({
+    channel: event.channel,
+    message_ts: event.ts,
+  });
+  console.log({ permalink });
+  //   const permalink = "";
+  //   await web.chat.postMessage({
+  //     channel: "suin_test",
+  //     text: `<${permalink}|\u{200B}>`,
+  //     unfurl_links: true,
+  //   });
   response.status(200).send("OK");
 };
