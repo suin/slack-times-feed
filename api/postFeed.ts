6ead840ffb9ca4dce7726f1dd1a2e80d28a7596d
suin <suinyeze@gmail.com>
2022-07-12 05:27:00 +0900

    feat: 🎸 add urlVerification

--- api/postFeed.ts
+++ api/postFeed.ts
@@ -1,6 +1,27 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+// import { WebClient } from "@slack/web-api";
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  console.log(request.body)
-  response.status(200).end()
+export default async (request: VercelRequest, response: VercelResponse) => {
+  console.log(request.body);
+  const { body } = request;
+
+  if (body.type === "url_verification") {
+    return urlVerification(request, response);
+  }
+
+  response.status(200).end();
+};
+
+// const postMessage = async () => {
+//   const web = new WebClient("");
+//   const permalink = "";
+//   await web.chat.postMessage({
+//     channel: "suin_test",
+//     text: `<${permalink}|\u{200B}>`,
+//     unfurl_links: true,
+//   });
+// };
+
+const urlVerification = (request: VercelRequest, response: VercelResponse) => {
+  response.status(200).send({ challenge: request.body.challenge });
 };
